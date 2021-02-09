444558c107b56ae2e48891d385f2d4566e4aad01
sun <545949068@qq.com>
2021-02-09 22:27:00 +0800

    deploy

---
+++ precache-manifest.bc1bbd1736426991fc789782612d4aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116b2a3e99ae2e3f639c",
+    "url": "/wm-tally/css/app.9a65dbfd.css"
+  },
+  {
+    "revision": "1fdf651e88c4edadcfae9027a25fdac2",
+    "url": "/wm-tally/index.html"
+  },
+  {
+    "revision": "116b2a3e99ae2e3f639c",
+    "url": "/wm-tally/js/app.e0244d61.js"
+  },
+  {
+    "revision": "c20093968d319265d58c",
+    "url": "/wm-tally/js/chunk-vendors.60073327.js"
+  },
+  {
+    "revision": "8bdcc7b1678706656cdda253f5ae224e",
+    "url": "/wm-tally/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wm-tally/robots.txt"
+  }
+]);
